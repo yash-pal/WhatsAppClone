43bf62e6caff128c947384b4700477d51e8538e6
Yash <Yash Pal>
2019-01-15 16:33:09 +0530

    Added files

---
+++ myapp/lib/whatsapp_home.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/pages/call_screen.dart';
+import 'package:myapp/pages/camera_screen.dart';
+import 'package:myapp/pages/chat_screen.dart';
+import 'package:myapp/pages/status_screen.dart';
+
+
+class WhatsAppHome extends StatefulWidget {
+  var cameras;
+  WhatsAppHome(this.cameras);
+
+  @override
+  _WhatsAppHomeState createState() => new _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    //  TODO: implement initState
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CHATS"),
+            new Tab(
+              text: "STATUS",
+            ),
+            new Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          new Icon(Icons.search),
+          new Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          new Icon(Icons.more_vert)
+        ],
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(widget.cameras),
+          new ChatScreen(),
+          new StatusScreen(),
+          new CallsScreen(),
+        ],
+      ),
+      floatingActionButton: buildFloatingActionButton(context),
+    );
+  }
+
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+    return new FloatingActionButton(
+      backgroundColor: Theme.of(context).accentColor,
+      child: new Icon(
+        Icons.message,
+        color: Colors.white,
+      ),
+      onPressed: () => print("open chats"),
+    );
+  }
+}
